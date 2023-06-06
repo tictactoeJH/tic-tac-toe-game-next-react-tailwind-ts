36c61a7d426fc2635c44e0d76f85add182e71f1e
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-06-06 17:32:12 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 'use client';
-import { useState } from 'react';
-import Board from './Board';
-
+import Game from './Game';
 export default function App() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const handlePlay = (newSquares: (string | null)[]) => {
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
   return (
     <>
-      <Board squares={squares} xIsNext={xIsNext} onPlay={handlePlay}/>
+      <Game/>
     </>
   );
 }
